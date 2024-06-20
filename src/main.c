5eb89e668b2be9cb5277a888711794574c754d2b
dikiyOpezdal <135449631+dikiyOpezdal@users.noreply.github.com>
2024-06-20 19:46:01 +0300

    light rotation, minor changes

--- src/main.c
+++ src/main.c
@@ -7,19 +7,11 @@ typedef struct {
 } vec3;
 
 typedef struct {
-    vec3 dir;
-    float dist;
-} camera_t;
-
-typedef struct {
     vec3 center;
     float radius;
 } sphere_t;
 
-typedef struct {
-    vec3 center;
-    float R, r;
-} torus_t;
+#define d2r 3.141592653589793f / 180.0f
 
 #define width  120
 #define height 30
@@ -34,14 +26,6 @@ sphere_t sphere = {{0.0f, 0.0f, 3.0f}, 2.0f};
 
 vec3 light_dir = {0.75f, -1.0f, 1.0f};
 
-vec3 translate(vec3 pos, vec3 dist) {
-    return (vec3){pos.x - dist.x, pos.y - dist.y, pos.z - dist.z};
-}
-
-vec3 rotate(vec3 dir0, vec3 dir1) {
-    return (vec3){dir0.x - dir1.x, dir0.y - dir1.y, dir0.z - dir1.z};
-}
-
 float magnitude(vec3 vec) {
     return sqrt(vec.x*vec.x + vec.y*vec.y + vec.z*vec.z);
 }
@@ -55,6 +39,28 @@ vec3 normalize(vec3 vec) {
 
     if (magn == 0) return vec;
     return (vec3){vec.x / magn, vec.y / magn, vec.z / magn};
+}
+
+vec3 rot_x(vec3 v0, double angle)
+{
+    vec3 v1 = v0;
+    v1.z = v0.z * cos(angle) - v0.y * sin(angle);
+    v1.y = v0.z * sin(angle) + v0.y * cos(angle);
+    return v1;
+}
+vec3 rot_y(vec3 v0, double angle)
+{
+    vec3 v1 = v0;
+    v1.x = v0.x * cos(angle) - v0.z * sin(angle);
+    v1.z = v0.x * sin(angle) + v0.z * cos(angle);
+    return v1;
+}
+vec3 rot_z(vec3 v0, double angle)
+{
+    vec3 v1 = v0;
+    v1.x = v0.x * cos(angle) - v0.y * sin(angle);
+    v1.y = v0.x * sin(angle) + v0.y * cos(angle);
+    return v1;
 }
 
 float hit_sphere(vec3 center, float radius, vec3 ray) {
@@ -74,28 +80,35 @@ char light2char(float light) {
 }
 
 int main() {
-    light_dir = normalize(light_dir);
+    vec3 rotation = (vec3){0.0f, 0.0f, 0.0f};
+    while (true) {
+        rotation.y += 0.05f*d2r;
 
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            float nx = ((float)x / width * 2.0f - 1.0f) * aspect * char_aspect;
-            float ny = (float)y / height * 2.0f - 1.0f;
+        vec3 trans_light = normalize(
+            rot_z(rot_y(rot_x(light_dir, rotation.x), rotation.y), rotation.z)
+        );
 
-            vec3 dir = normalize((vec3){nx, ny, 1.0f});
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                float nx = ((float)x / width * 2.0f - 1.0f) * aspect * char_aspect;
+                float ny = (float)y / height * 2.0f - 1.0f;
 
-            float t = hit_sphere(sphere.center, sphere.radius, dir);
-            if (t > 0) {
-                vec3 normal = normalize((vec3){t * dir.x, t * dir.y, t * dir.z + 1.0f});
-                float light = dot(normal, light_dir);
-                if (light < 0.0f) light = 0.0f;
-                screen[y * width + x] = light2char(light);
+                vec3 dir = normalize((vec3){nx, ny, 1.0f});
+
+                float t = hit_sphere(sphere.center, sphere.radius, dir);
+                if (t > 0.0f) {
+                    vec3 normal = normalize((vec3){t * dir.x, t * dir.y, t * dir.z + 1.0f});
+                    float light = dot(normal, trans_light);
+                    if (light < 0.0f) light = 0.0f;
+                    screen[y * width + x] = light2char(light);
+                }
+                else screen[y * width + x] = light2char(0.0f);
             }
-            else screen[y * width + x] = light2char(0.0f);
         }
-    }
 
-    screen[width * height] = '\0';
-    printf("%s", screen);
+        screen[width * height] = '\0';
+        printf("%s", screen);
+    }
 
     return 0;
 }
